5606494042b4f4fb39376e76870ecf177e83344b
Lojain Mostafa <lojain17mostafa@gmail.com>
2023-11-29 18:58:19 +0200

    main

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/responsive/responsive_layout_screens.dart';
+import 'package:instagram/screens/login_screen.dart';
+import 'package:instagram/screens/signup_screen.dart';
+import 'package:instagram/utils/colors.dart';
+import 'responsive/mobile_screen_layout.dart';
+import 'responsive/web_screen_layout.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      // ignore: prefer_const_constructors
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: SignupScreen(),
+    );
+  }
+}
